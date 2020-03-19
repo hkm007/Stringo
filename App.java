80e591fea5e95296013ffe261c7dea3d7895cc96
Himanshu Mishra <48494012+hkm007@users.noreply.github.com>
2020-03-19 19:38:24 +0530

    vowels/consonant feature added

--- App.java
+++ App.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 
 public class App extends JFrame
 {
-	JLabel l1, l2, l3, l4, l5;
+	JLabel l1, l2, l3, l4, l5, l6;
 	JTextField t1;
 	JButton b1;
 
@@ -19,6 +19,7 @@ public class App extends JFrame
 		l3 = new JLabel();
 		l4 = new JLabel();
 		l5 = new JLabel();
+		l6 = new JLabel();
 		t1 = new JTextField();
 		b1 = new JButton("Submit");
 
@@ -30,6 +31,7 @@ public class App extends JFrame
 		l3.setBounds(50, 170, 200, 20);
 		l4.setBounds(50, 190, 300, 20);
 		l5.setBounds(50, 210, 300, 20);
+		l6.setBounds(50, 230, 300, 20);
 
 		b1.addActionListener(new Handler());
 
@@ -38,6 +40,7 @@ public class App extends JFrame
 		add(l3);
 		add(l4);
 		add(l5);
+		add(l6);
 		add(t1);
 		add(b1);
 	}
@@ -50,9 +53,18 @@ public class App extends JFrame
 			int len = text.length();
 			String res1 = text.toUpperCase();
 			String res2 = text.toLowerCase();
+			int v = 0, c = 0;
+			for(int i = 0; i < len; i++)
+			{
+				if(text.charAt(i) == 'a' || text.charAt(i) == 'A' || text.charAt(i) == 'e' || text.charAt(i) == 'E' || text.charAt(i) == 'i' || text.charAt(i) == 'I' || text.charAt(i) == 'o' || text.charAt(i) == 'O' || text.charAt(i) == 'u' || text.charAt(i) == 'U')
+					v++;
+				else if(text.charAt(i) != ' ') 
+					c++;
+			}
 			l3.setText("Length of string: " + len);
 			l4.setText("Upper Case: " + res1);
 			l5.setText("Lower Case: " + res2);
+			l6.setText("Vowels/Consonants: " + v + "/" + c);
 		}
 	}
  
